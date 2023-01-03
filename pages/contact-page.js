19565200c6afca6e6b17130792ce670c5298e9ca
h_yabiku <>
2023-01-04 07:32:02 +0900

    Contactページ作成

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,10 +1,24 @@
 import Layout from '../components/Layout'
-
+import Image from 'next/image'
+  
 const Contact = () => {
   return (
-    <Layout>
-      <div>
-        Contact page
+    <Layout title='Contact'>
+      <div className='bg-white text-center shadow-xl p-8 w-80 rounded'>
+        <div className='mt-4'>
+          <p className='font-bold'>Contact info</p>
+        </div>
+        <div className='flex justify-center'>
+          <Image className='rounded-full' src='/avatar.jpg' width={60} height={60} alt="Avator" />
+
+        </div>
+        <div className='mt-4'>
+          <p className='font-bold'>Email</p>
+          <p>
+            <a href='mailto:
+            ' className='text-blue-500'>@gmail.com</a>
+          </p>
+        </div>
       </div>
     </Layout>
   )
