22b4ec72f1c9b99ac9bde463a241dd4e79572786
h_yabiku <>
2023-01-05 06:46:29 +0900

    add: blog page

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,13 +1,28 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { getAllPostsData } from '../lib/posts';
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
-     <Layout>
-      <div>
+    <Layout title='Blog'>
+      <ul className='m-5'>
+        {posts && posts.map((post) => (
+          <li key={post.id}>
+            <span>{post.id}</span>
+            :{" "}
+            <span>{post.title}</span>
+          </li>
+        ))}
+      </ul>
         Blog page
-      </div>
      </Layout>
   )
 }
 
 export default Blog
+
+export const getStaticProps = async () => {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout'
 import Image from 'next/image'
-  
+
 const Contact = () => {
   return (
     <Layout title='Contact'>
@@ -10,7 +10,6 @@ const Contact = () => {
         </div>
         <div className='flex justify-center'>
           <Image className='rounded-full' src='/avatar.jpg' width={60} height={60} alt="Avator" />
-
         </div>
         <div className='mt-4'>
           <p className='font-bold'>Email</p>
